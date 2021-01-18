5add09e7687818af2e011e828e66a3ec5c76ac91
JunHong30 <neojunhong2003@gmail.com>
2021-01-18 11:37:36 +0800

    Initial commit

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public float Speed;
+    public float rotateSpeed;
+
+    public GameObject Bullet;
+    public GameObject BulletSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * Speed);
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(new Vector3(0, Time.deltaTime * rotateSpeed, 0));
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector3.back * Time.deltaTime * Speed);
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(new Vector3(0, Time.deltaTime * -rotateSpeed, 0));
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(Bullet, BulletSpawn.transform.position, transform.rotation);
+        }
+    }
+}
